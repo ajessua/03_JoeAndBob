b2312b0e443d2d760b300090093d6454e3de6bb6
Amanda M. Jessup <mandajessup@gmail.com>
2021-02-21 20:31:03 -0500

    Add project files.

---
+++ JoeAndBob/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace JoeAndBob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe = new Guy { cash = 50, name = "Joe" };
+            Guy bob = new Guy { cash = 100, name = "Bob" };
+
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+
+                if (howMuch == "") return;
+
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+
+                    if (whichGuy == "Joe")
+                    {
+                        int cashGiven = joe.GiveCash(amount);
+                        bob.ReceiveCash(cashGiven);
+                        Console.Clear();
+
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        int cashGiven = bob.GiveCash(amount);
+                        joe.ReceiveCash(cashGiven);
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob'");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
+
+        }
+    }
+}
